d816a8b3dd9734080b1edf259535f85634277f6a
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2025-06-01 18:04:59 +0200

    Add support for secrets in Cloud Run configuration and update IAM roles

--- terraform/cloudRun.tf
+++ terraform/cloudRun.tf
@@ -11,6 +11,9 @@ module "cloud_run_dashboardManagement" {
   env_variables = {
     NODE_ENV = "production"
   }
+  secrets = {
+    DATABASE_URL = "DATABASE_URL_PROD"
+  }
 }
 
 module "cloud_run_dashboardManagement_invokers" {
