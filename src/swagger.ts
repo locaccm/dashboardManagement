0337407041b155fbbf3515d01d5261cff90ba97e
moslehSN <mosleh.snoussi49@gmail.com>
2025-06-11 17:16:15 +0200

    feat : [AIC-38]-add swagger

---
+++ src/swagger.ts
@@ -0,0 +1,22 @@
+// src/swagger.ts
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Dashboard Management API",
+      version: "1.0.0",
+      description: "API documentation for dashboard management microservice",
+    },
+  },
+  apis: ["./src/**/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export function setupSwagger(app: Express) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
